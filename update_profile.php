5a808760b409bcbd7288592f77feba964058671f
nthnhi1209 <58299550+nthnhi1209@users.noreply.github.com>
2019-12-12 19:36:53 +0700

    Update update_profile.php

--- update_profile.php
+++ update_profile.php
@@ -9,19 +9,19 @@
 ?>
 <?php include 'header.php'; ?>
 <h1> Cập nhật thông tin cá nhân </h1>
-<?php if (isset($_POST['displayName'])) : ?>
+<?php if (isset($_POST['displayName']) && isset($_POST['phoneNumber'])) : ?>
 <?php
     
     $displayName= $_POST['displayName'];
-    
+    $phoneNumber = $_POST['phoneNumber'];
     $success =false;
-
     if($displayName !=''){
-      updateUserProfile($currentUser['id'],$displayName);
+      $displayName= $currentUser['displayName'];
+      updateUserProfile($currentUser['id'],$displayName, $phoneNumber);
       $success=true;
     }
 
-    if (isset($_FILES['avatar'])){
+   if (isset($_FILES['avatar']) && $_FILES['avatar']['name']){
       $success =false;
       $file = $_FILES['avatar'];
       $fileName = $file['name'];
@@ -31,7 +31,11 @@
       $filePath = './avatars/' . $currentUser['id'] . '.jpg';
       $success = move_uploaded_file($fileTemp, $filePath);
       $newImage = resizeImage($filePath, 400, 400);
-      imagejpeg($newImage, $filePath);
+      ob_start();
+      imagejpeg($newImage);
+      $data= ob_get_contents();
+      ob_end_clean();
+      $success =true;
     }
 ?>
 <?php if ($success): ?>
@@ -44,11 +48,18 @@
 <?php else: ?>
 <form  method="POST" enctype="multipart/form-data">    
     <div class ="form-group">
-        <label for="displayName"><strong>Họ tên</strong></label>
-        <input type ="text"class = "form-control"id ="displayName" name="displayName" placeholder="Họ tên " value="<?php echo $currentUser['displayName']; ?>">
+        <label for="displayName"><strong> Họ tên</strong></label>
+        <input type ="text"class = "form-control"id ="displayName" name="displayName" aria-describedby="numHelp" value="Họ tên " value="<?php echo $currentUser['displayName']; ?>">
+        <small id="numHelp" class ="form-text text-muted"> Hãy nhập họ tên cần thay đổi !!<
     </div>
+    <div class="alert alert-danger" role="alert">
+        <label for="phoneNumber">Số Điện Thoại</strong></label>
+        <input type ="text"class = "form-control"id ="phoneNumber" name="phoneNumber"aria-describedby="numHelp" placeholder="<?php echo $currentUser['phoneNumber']; ?>">
+        <small id="numHelp" class ="form-text text-muted"> Hãy nhập số điện thoại cần thay đổi !!<
+    </div>
+    <div class="alert alert-danger" role="alert">
     <div class ="form-group">
-        <label for="avatar"><strong> Ảnh đại diện</strong></label>
+        <label for="avatar"><strong> Đổi Ảnh Avatar</strong></label>
         <input type ="file"class = "file"id ="avatar" name="avatar">
     </div>
 
