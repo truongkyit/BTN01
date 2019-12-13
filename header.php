277105065b785c01b2e9cfe394194678a3c917ba
Lê Nguyễn Trường Kỳ <33385306+truongkyit@users.noreply.github.com>
2019-12-13 16:39:43 +0700

    Update header.php

--- header.php
+++ header.php
@@ -6,11 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet"> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <script src="js/jquery-1.11.1.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <title> SUNSHINE</title>
+    <title>1760090</title>
 
     <style>
             *{
@@ -37,16 +35,64 @@
                     <button type="button" class="btn btn-danger btn-sm"
                     style="margin-top:30px" onclick="location.href='index.php';">HOME</button>
                 </div>
+                
                 <div class="col-xs-12 col-sm-12 col-md-2">
-                    <button type="button" class="btn btn-danger btn-sm"
-                    style="margin-top:30px" onclick="location.href='register.php';">Đăng kí</button>
+                    <button type="button" class="btn btn-success" style="margin-top:30px" data-toggle="modal" data-target="#registerModal">Đăng ký</button>
+                </div>
+                <div class="modal fade" role="dialog" id="registerModal">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h3 class="modal-title">Đăng ký</h3>
+                                <button type="button" class="close" data-dismiss="modal" >&times;</button>
+                            </div> 
+
+                            <div class="modal-body">
+                            <form action="register.php" method="POST">
+                                <div class="col-md-4">
+                                    <input type="text" class="form-control" id ="displayName" name="displayName" placeholder="Your name">       
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                                </div> 
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-primary">Đăng ký</button>
+                                </div>      
+                            </form>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
 
-                <?php if (!$currentUser): ?>
+                <?php if (!$currentUser):?>
                 <div class="col-xs-12 col-sm-12 col-md-2">
-                    <button type="button" class="btn btn-danger btn-sm"
-                    style="margin-top:30px" onclick="location.href='login.php';">Đăng nhập</button>
+                    <button type="button" class="btn btn-success" style="margin-top:30px" data-toggle="modal" data-target="#loginModal">Đăng nhập</button>
                 </div>
+                <div class="modal fade" role="dialog" id="loginModal">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h3 class="modal-title">Đăng nhập</h3>
+                                <button type="button" class="close" data-dismiss="modal" >&times;</button>
+                            </div> 
+
+                            <div class="modal-body">
+                            <form action="login.php" method="POST">
+                                <div class="col-md-4">
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                
+                                </div> 
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-primary">Đăng nhập</button>
+                                </div>      
+                            </form>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                
                 <?php else: ?>
 
                 <div class="col-xs-12 col-sm-12 col-md-2">
@@ -67,3 +113,8 @@
                 <?php endif; ?>
                 </div>
         </nav>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
