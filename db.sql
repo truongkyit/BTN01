b968d823f67627ceded0010c6f21a5e79f078181
hoanhi <hoanhi1209@gmail.com>
2019-12-27 23:35:31 +0700

    Commit db

--- db.sql
+++ db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th12 15, 2019 lúc 08:15 AM
+-- Thời gian đã tạo: Th12 27, 2019 lúc 04:24 PM
 -- Phiên bản máy phục vụ: 5.7.26
--- Phiên bản PHP: 7.2.18
+-- Phiên bản PHP: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `postId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pk_comment_users` (`userId`),
+  KEY `pk_comment_posts` (`postId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `friendship`
+--
+
+DROP TABLE IF EXISTS `friendship`;
+CREATE TABLE IF NOT EXISTS `friendship` (
+  `userid1` int(11) NOT NULL,
+  `userid2` int(11) NOT NULL,
+  `createAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userid1`,`userid2`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE IF NOT EXISTS `likes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `postId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `posts`
 --
 
@@ -34,6 +80,8 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `userId` int(11) NOT NULL,
   `createAT` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `img` blob NOT NULL,
+  `likes` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `pk_posts_users` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -52,12 +100,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '0',
   `code` varchar(16) NOT NULL,
+  `phoneNumber` varchar(100) NOT NULL,
+  `avatar` blob NOT NULL,
   PRIMARY KEY (`id`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
 --
+
+--
+-- Các ràng buộc cho bảng `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `pk_comment_posts` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `pk_comment_users` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
 
 --
 -- Các ràng buộc cho bảng `posts`
